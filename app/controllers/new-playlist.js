33996b670d47d2d87f8ea6d471a7fe261b2ec2ed
Cory <crbi2550@gmail.com>
2015-07-21 18:12:04 -0700

    First attempt at adding playlist to firebase.io with info of newly added user playlist

--- app/controllers/new-playlist.js
+++ app/controllers/new-playlist.js
@@ -4,18 +4,18 @@ export default Ember.Controller.extend({
   actions: {
     submit: function() {
 
-      // SC.initialize({
-      //   client_id: 'e0d5f2931e81cf14facf65268cd656e0',
-      //   redirect_uri: "http://localhost:4200"
-      // });
-
+      var newPlaylist = this.store.createRecord('playlist');
       var playlistName = this.get('title');
       SC.post('/playlists', {
        playlist: { title: playlistName, tracks: [] }
-      },
-      function(response) {
-        console.log(response)
-      });
+      }, function(response) {
+          newPlaylist.set('name', playlistName);
+          newPlaylist.set('playlist_id', response.id);
+          newPlaylist.set('playlist_uri', response.uri);
+        }
+      );
+      newPlaylist.save();
+      this.transitionToRoute('songs');
     }
   }
 });
--- app/models/playlist.js
+++ app/models/playlist.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-
-  
+  playlist_id: DS.attr('number'),
+  playlist_uri: DS.attr('string')
 
 });
