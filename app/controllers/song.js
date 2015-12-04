3320e3abf723bdd9ed7c75725c1f414f94442788
Cory <crbi2550@gmail.com>
2015-12-04 01:24:41 -0800

    Delete from playlist functionality

--- app/controllers/song.js
+++ app/controllers/song.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  
+  actions: {
+    delete: function() {
+      this.get('model').destroyRecord().then(() => {
+        this.transitionToRoute('playlists');
+      });
+    }
+  }
 });
